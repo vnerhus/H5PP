aab5b9fc34528d193290502bddbc85f4775200c6
vegardn <vegardn@gmail.com>
2020-10-07 17:54:23 +0200

    Trying to get json loads to load content.

--- h5pp/h5p/library/h5pclasses.py
+++ h5pp/h5p/library/h5pclasses.py
@@ -1050,7 +1050,7 @@ class H5PCore:
 
         # Validate and filter against main library semantics.
         validator = H5PContentValidator(self.h5pF, self)
-        content['params'] = content['params'].replace("\\", "").replace(":", ": ")
+        content['params'] = content['params'].replace("\\", "").replace(":", ": ").replace(",", ", ")
         content['params'] = eval(content['params']).decode('utf-8')
         print(content['params'])
         print("{'answers': 'hello'}")
