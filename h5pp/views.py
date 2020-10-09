37452d7cf08a9e06a7ac220f6518c46daf5ca276
vegardn <vegardn@gmail.com>
2020-10-09 10:50:23 +0200

    getUserScore try except

--- h5pp/views.py
+++ h5pp/views.py
@@ -172,7 +172,10 @@ def embedView(request):
         if request.user.is_authenticated or True:
             h5pSetStarted(request.user, h5pGetContentId(request))
             #score = getUserScore(request.GET.get('contentId', 21), request.user)[0]
-            score = getUserScore(21, request.user)[0]
+            try:
+                score = getUserScore(21, request.user)[0]
+            except:
+                score = 0
         return render(request, 'h5p/embed.html', {'embed': embed, 'score': score})
 
     return HttpResponseForbidden()
